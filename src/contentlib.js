d557d1c0b5bd552f263d46e872441f85cbf99fd0
zak Harris <zachary.harris314@protonmail.com>
2021-04-19 14:41:09 -0400

    third commit

--- src/contentlib.js
+++ src/contentlib.js
@@ -1,35 +1,48 @@
 import { RestaurantRounded } from "@material-ui/icons";
-import React , {useState, useEffect }from "react";
+import React, { useState, useEffect } from "react";
 import "./contentlib.js";
 
 function Content({ id }) {
-  const [content,setContent] = useState([])
+  const [content, setContent] = useState([]);
 
-  const fetchdata = () => {
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
+  const fetchdata = async () => {
     let url = null;
-    switch (ID) {
+    switch (id) {
       case 3:
         url = "http://localhost:8000/api/info/1";
         break;
       case 4:
         url = "http://localhost:8000/api/info/2";
         break;
+      default:
+        break;
     }
-    let response = await fetch(url);
-    let content = await response.json();
-    setContent(content);}
+    const ContentSet = async (content) => {
+      setContent(content);
+    };
 
-  useEffect(() =>{
-  fetchdata();
-  })
+    let response = await fetch(url, {
+      mode: "cors",
+      headers: { "Access-Control-Allow-Origin": "*" },
+    });
+    let content = await response.json();
+    let Cont = await ContentSet(content);
+    console.log(Cont);
+  };
 
   return (
     <div className="content-slide library " id="content">
       {content.map((cont) => {
-        const {id,image,category} = cont
+        const { id, image, category } = cont;
         return (
-          <img src={`http://localhost:8000/api/category/${category}/id/${id}`}></img>
-        )
+          <img
+            src={`http://localhost:8000/api/category/${category}/id/${id}`}
+          ></img>
+        );
       })}
     </div>
   );
